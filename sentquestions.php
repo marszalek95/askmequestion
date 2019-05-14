637d90626553ce1bd72fdb599f1a6c8d4fdc112b
Adam Marszalek <adam.marszalek95@gmail.com>
2019-05-14 16:19:43 +0200

    first commit

---
+++ sentquestions.php
@@ -0,0 +1,72 @@
+<?php include 'core/init.php'; ?>
+<?php if(!$session->is_signed_in()) {redirect("index.php");} ?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+<!--    <link rel="icon" href="../../../../favicon.ico">-->
+
+    <title>AskMeQuestion</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/styles.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+      
+
+    <?php include 'includes/navbar.php'; ?>
+
+      
+      
+      <?php $questions = Question::find_questions_by_status($session->user_id, "status = 0"); ?>
+      <main role="main" class="cover h-100 p-3">
+      <div class="d-flex p-3 row">
+      <?php foreach($questions as $question) : ?>
+      <?php if($question->add_by == $session->user_id) : ?>
+      <?php $friend = User::find_by_id($question->add_to); ?>
+          <div class="col-sm-6">
+              <div class="card card-hover text-white bg-dark border-secondary mb-3" data-clickable="true" data-href="question.php?id=<?php echo $question->id; ?>" style="max-width: 20rem;">
+             <div class="card-header">To <?php echo $friend->username; ?></div>
+             <div class="card-body">
+               <p class="card-text"><?php echo $question->question; ?></p>
+             </div>
+           </div>
+          </div>
+      <?php endif; ?>
+      <?php endforeach; ?>
+      </div>
+      </main>
+    
+      
+      
+      
+      
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Copyright &copy; Adam Marszalek 2019</p>
+        </div>
+      </footer>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/javascript.js"></script>
+  </body>
+</html>

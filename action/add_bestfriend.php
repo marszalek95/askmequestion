636df4237f0010e77587d402784e17ccbaf5a449
Adam Marszalek <adam.marszalek95@gmail.com>
2019-08-22 19:47:00 +0200

    some improvements

--- action/add_bestfriend.php
+++ action/add_bestfriend.php
@@ -9,17 +9,17 @@ $friend = Friends::find_by_id($id);
 
 if($friend->user_one_id == $session->user_id)
 {
-    if($friend->status_user_two == 1)
+    if($friend->status_user_two == 2)
     {
-        $friend->update_status('status_user_two', 4);
+        $friend->update_status('status_user_two', 3);
         redirect($_SERVER['HTTP_REFERER']);
     }
 }
 elseif($friend->user_two_id == $session->user_id)
 {
-    if($friend->status_user_one == 1)
+    if($friend->status_user_one == 2)
     {
-        $friend->update_status('status_user_one', 4);
+        $friend->update_status('status_user_one', 3);
         redirect($_SERVER['HTTP_REFERER']);
     }
 }
